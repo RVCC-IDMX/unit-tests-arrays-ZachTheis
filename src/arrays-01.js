b141a56d29c985037adbef609c824309290b0469
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-09-30 22:48:31 -0400

    Change arrays-01
    Add code to arrays-01.js to pass all tests

--- src/arrays-01.js
+++ src/arrays-01.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-param-reassign */
 /*
  * arrays-01.js
@@ -24,7 +25,7 @@
  * ? https://bit.ly/3EUI0j9 - Reading and Writing Array Elements
  */
 function getFirstValue(arr) {
-  // write your code here & return value
+  return arr[0];
 }
 
 /**
@@ -36,7 +37,8 @@ function getFirstValue(arr) {
  * @returns {array} - array of the 4 elements
  */
 function makeArray(a, b, c, d) {
-  // write your code here & return value
+  const newArray = [a, b, c, d];
+  return newArray;
 }
 
 /**
@@ -47,7 +49,8 @@ function makeArray(a, b, c, d) {
  *
  */
 function createFirstFivePrimes() {
-  // write your code here & return value
+  const primeArray = [2, 3, 5, 7, 11];
+  return primeArray;
 }
 /**
  * Creates a new array with a number of elements
@@ -58,7 +61,12 @@ function createFirstFivePrimes() {
  * ? must use a for loop
  */
 function createSizedArray(size) {
-  // write your code here & return value
+  const newArray = [];
+
+  for (let i = 0; i < size; i++) {
+    newArray.push(i);
+  }
+  return newArray;
 }
 
 /**
@@ -69,7 +77,9 @@ function createSizedArray(size) {
  * ? https://bit.ly/39z4FTE - Adding and Deleting Array Elements
  */
 function modifyArrayByAdding(arr) {
-  // write your code here & return value
+  arr.push(1);
+  arr.unshift(1);
+  return arr;
 }
 
 /**
@@ -79,7 +89,9 @@ function modifyArrayByAdding(arr) {
  * ? example: [1, 2, 3, 4, 5] => [2, 3, 4]
  */
 function modifyArrayByDeleting(arr) {
-  // write your code here & return value
+  arr.shift();
+  arr.pop();
+  return arr;
 }
 
 /**
@@ -90,7 +102,7 @@ function modifyArrayByDeleting(arr) {
  * ? example: [1, 2, 3, 4, 5], index = 2 => 3
  */
 function findElementAtIndex(arr, index) {
-  // write your code here & return value
+  return arr[index];
 }
 
 /**
@@ -102,7 +114,7 @@ function findElementAtIndex(arr, index) {
  * ? hint: https://bit.ly/2ZyFaAj - Array Searching and Sorting Methods
  */
 function findElementByValue(arr, value) {
-  // write your code here & return value
+  return arr.indexOf(value);
 }
 
 /**
@@ -114,7 +126,8 @@ function findElementByValue(arr, value) {
  * ? example: [1, 2, 3, 4, 5], index = 2, value = 0 => [1, 2, 0, 4, 5]
  */
 function changeElementAtIndex(arr, index, value) {
-  // write your code here & return value
+  arr[index] = value;
+  return arr;
 }
 
 /**
@@ -125,7 +138,7 @@ function changeElementAtIndex(arr, index, value) {
  * ? https://mzl.la/3ib7Dm3
  */
 function makeStringFromArray(arr) {
-  // write your code here & return value
+  return arr.join('');
 }
 
 /**
@@ -136,7 +149,8 @@ function makeStringFromArray(arr) {
  * ? https://bit.ly/39ASLc0 - The spread operator
  */
 function createShallowCopy(arr) {
-  // write your code here & return value
+  const arrayClone = [...arr];
+  return arrayClone;
 }
 
 module.exports = {
